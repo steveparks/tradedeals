63c2890e68cb5aef4c5a8836467d39be30cf9bdb
Steve Parks <steve.parks@weareconvivio.com>
2018-01-07 11:47:43 +0000

    Add practical step suggestions

--- src/pages/index.js
+++ src/pages/index.js
@@ -68,6 +68,23 @@ export default ({ data }) => {
       </ul>
       <p>Out of those 759+ agreements that will be needed, the UK government has 0 and has started formal talks with 0 nations. This site will update as trade deals are announced.</p>
       <h2>Brexit spells disaster for the UK, and our government has failed to prepare. It is our patriotic duty to protect the UK by stopping Brexit.</h2>
+      <p>So what can you do?</p>
+      <ul>
+        <li>There are some public interest legal cases that you can support:
+          <ul>
+            <li><a href="https://www.crowdjustice.com/case/a50-chall-her-e50/">Challenge the unconstitutional notification of Article 50</a>.</li>
+            <li><a href="https://www.crowdjustice.com/case/secretbrexitstudies/">Force full disclouse of the Brexit 'impact studies'</a>.</li>
+            <li><a href="https://www.crowdjustice.com/case/did-vote-leave-break-spending-limits/">Bring Vote Leave to account for allegedly breaking campaign spending rules</a>.</li>
+            <li><a href="https://www.crowdjustice.com/case/strengthening/">Ensure the government allows parliameng to vote on all options</a>.</li>
+          </ul>
+        </li>
+        <li><a href="https://www.stopbrexitmarch.com/">Take part in marches and campaigns across the UK to Stop Brexit</a>.</li>
+        <li><a href="https://twitter.com/stopbrexit?lang=en">Follow @stopbrexit on Twitter for other news</a>.</li>
+        <li>Although it can be hard, do follow the news (from reputable sources).</li>
+        <li>Talk to family and friends about it to ensure people know the reality of what Brexit is going to mean. Public opinion is shifting, and we can all help that trend continue.</li>
+      </ul>
+      <p>There are good people out there doing good work. You are not alone, and you can be part of the movement to Stop Brexit or at least reduce its harmful impact.</p>
+      <h1>Love Britain - Stop Brexit</h1>
       </div>
     )
 }
